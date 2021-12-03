71e19056cfd45897873a66e88d18a5b0e51b0b85
Aniket Khaire <anic10@Anikets-MacBook-Pro.local>
2021-12-03 23:04:11 +0530

    Inital commit

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# percydemo
+
+# Percy Assignment
+
+Using Percy to perform visual comparison between two environments URLs
+
+1) Prod - https://www.browserstack.com
+
+2) Non-Prod - https://k8s.bsstag.com/ 
+## Endpoints to compare
+- 1 /
+
+- 2 /pricing
+
+- 3 /integrations/automate
+
+- 4 /docs 
+## Browsers used
+
+- 1 . Firefox 
+- 2 . Chrome
+- 3 . Safari 
+
+## Documentation
+
+[Percy document to compare two environment](https://docs.percy.io/docs/comparing-two-urls-to-each-other)
+
+[Assignment link](https://browserstack.atlassian.net/wiki/spaces/CE/pages/1812693491/Technical+Product+Training)
+
+[Percy docs](https://docs.percy.io/docs/cypress)
+## Environment Variables
+
+To run this project, you will need to set the token
+
+`PERCY Token` : Percy app>  Projet setting
+
+
+
+##  Steps:
+
+- 1 Login into percy and create a demo account
+- 2 For installing necessary dependencies follow percy document in the documentation
+- 3 Create a test script for above assignment using any SDK
+- 4 Create two branches i.e. PERCY_BRANCH and PERCY_TARGET_BRANCH
+- 5 PERCY_BRANCH = https://www.browserstack.com, PERCY_TARGET_BRANCH = https://k8s.bsstag.com/ 
+- 6 Link the repo with the percy app > integrations, in the project setting we can change the branch settings keep the default base branch = PERCY_BRANCH and auto approve branch = PERCY_BRANCH
+- 7 Before running the test - use export token command, which can be found in Percy app-> Project_setting
+- 8 Run the test using the following command - npm run test and push the code on PERCY_BRANCH
+- 9 Change the url, run the test and push the code to PERCY_TARGET_BRANCH
+- 10 CHeck on percy app, there will be comparision of the two builds.
+
+
+## Technologies used:
+
+- 1  Cypress
+- 2  Percy
+## Authors
+
+- [@Anic10](https://www.github.com/octokatherine)
+
---
+++ cypress/integration/testspec.js
@@ -0,0 +1,59 @@
+describe('Integration test with visual testing', function() {
+  context('Running in resolution', () => {
+    beforeEach(() => {
+      cy.viewport(1280, 720)
+    })
+
+    it('Loads the homepage', function() {
+      // Load the page or perform any other interactions with the app.
+      cy.visit(Cypress.env('baseurl'));
+      cy.wait(1000);
+
+      //Verifying the content
+      cy.get('h1').should('have.text', 'App & Browser Testing Made Easy')
+      cy.wait(1000);
+
+      // Take a snapshot for visual diffing
+      cy.percySnapshot('Homepage' ,{ widths: [375, 480, 720, 1280, 1440, 1920] })
+    });
+
+    it('Loads the Pricing page', function() {
+      // Load the page or perform any other interactions with the app.
+      cy.visit(Cypress.env('baseurl')+'/pricing');
+      cy.wait(1000);
+
+      //Verifying the content
+      cy.get('.plans-wrapper > :nth-child(1) > .live-element').should('have.text', 'Replace your device lab and VMs with any of these plans')
+      cy.wait(1000);
+
+      // Take a snapshot for visual diffing
+      cy.percySnapshot('Amount page',{ widths: [375, 480, 720, 1280, 1440, 1920] })
+    });
+
+    it('Loads the Integration autonmate page', function() {
+      // Load the page or perform any other interactions with the app.
+      cy.visit(Cypress.env('baseurl')+"/integrations/automate");
+      cy.wait(1000);
+
+      //Verifying the content
+      cy.get('h1').should('have.text', 'Integrations that make life easier')
+      cy.wait(1000);
+
+      // Take a snapshot for visual diffing
+      cy.percySnapshot('App page',{ widths: [375, 480, 720, 1280, 1440, 1920] })
+    });
+
+    it('Loads the Documents page', function() {
+      // Load the page or perform any other interactions with the app.
+      cy.visit(Cypress.env('baseurl')+"/docs");
+      cy.wait(1000);
+
+      //Verifying the content
+      cy.get('h1').should('have.text', 'Documentation')
+      cy.wait(1000);
+
+      // Take a snapshot for visual diffing
+      cy.percySnapshot('Help page'),{ widths: [375, 480, 720, 1280, 1440, 1920] }
+    });
+  });
+});
